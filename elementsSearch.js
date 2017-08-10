55ba654077b61c718f468a39d3b4f6326ab4dceb
Evgeniy Chernov <evgeniy.chernov@gmail.com>
2017-08-10 18:40:57 +0400

    elementsSearch

---
+++ elementsSearch.js
@@ -0,0 +1,7 @@
+var labels = document.querySelectorAll('table > label'),
+    tr = document.querySelector('#age-table > tr > td'),
+    form = document.body.children[1],
+    name = document.getElementsByName('search'),
+    input = document.querySelector('input[name="search"]'),
+    info1 = document.getElementsByName('info[0]'),
+    info2 = document.querySelector('form[name="search-person"] [name="info[0]"]');
