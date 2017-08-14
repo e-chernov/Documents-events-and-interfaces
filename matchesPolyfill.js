300436df767c23750a238f974953c39fe2952b28
Evgeniy Chernov <evgeniy.chernov@gmail.com>
2017-08-14 18:51:41 +0400

    matchesPolyfill

---
+++ matchesPolyfill.js
@@ -0,0 +1,6 @@
+if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.matchesSelector ||
+       Element.prototype.webkitMatchesSelector ||
+       Element.prototype.mozMatchesSelector ||
+       Element.prototype.msMatchesSelector;
+}
